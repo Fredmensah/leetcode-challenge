45514eb1fbd3c4be6627190d8a91e38126eab073
DevKobby <fred_obiriyeboah@yahoo.com>
2021-04-12 19:51:46 +0000

    Create two-sum.js
    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
    
    You may assume that each input would have exactly one solution, and you may not use the same element twice.
    
    You can return the answer in any order.
    
     
    
    Example 1:
    
    Input: nums = [2,7,11,15], target = 9
    Output: [0,1]
    Output: Because nums[0] + nums[1] == 9, we return [0, 1].
    Example 2:
    
    Input: nums = [3,2,4], target = 6
    Output: [1,2]
    Example 3:
    
    Input: nums = [3,3], target = 6
    Output: [0,1]
     
    
    Constraints:
    
    2 <= nums.length <= 103
    -109 <= nums[i] <= 109
    -109 <= target <= 109
    Only one valid answer exists.

---
+++ two-sum.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const validValues = [];
+    
+    for (const num of nums) {
+        const numIndex = nums.indexOf(num);
+        let selectedIndex = 0;
+
+        while (selectedIndex < (nums.length)) {     
+            const firstValue = nums[numIndex];
+            const secondValue = nums[selectedIndex];
+            
+            if (numIndex !== selectedIndex && firstValue !== secondValue) {
+                if ((firstValue + secondValue) === target) {
+                    return [numIndex , selectedIndex]; 
+                }
+            }
+
+            selectedIndex++;
+        }
+        
+    }
+};
